040a324289a2b048e9bc8f07f0583e805aae7c65
httpfatimaaa <86741036+httpfatimaaa@users.noreply.github.com>
2023-06-14 21:43:29 +0300

    Add files via upload

---
+++ utils/complexity_metrics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+def pixel_count_complexity(picture):
+    """
+    Computes fraction of black pixels in image
+    """
+    return np.mean(picture)
+
+
+# In[ ]:
+
+
+
+
